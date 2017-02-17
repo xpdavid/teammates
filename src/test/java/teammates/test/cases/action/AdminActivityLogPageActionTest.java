00e16a3f6fa02069c4df8a2c26c83ee24d753634
Pu Xiao <xp.xiaopu@gmail.com>
2017-02-18 03:38:38 +0800

    success

--- src/test/java/teammates/test/cases/action/AdminActivityLogPageActionTest.java
+++ src/test/java/teammates/test/cases/action/AdminActivityLogPageActionTest.java
@@ -2,8 +2,16 @@ package teammates.test.cases.action;
 
 import org.testng.annotations.Test;
 
+import com.google.appengine.api.log.LogQuery;
+import com.google.appengine.api.log.LogService;
+import com.google.appengine.api.log.LogServiceFactory;
+import com.google.appengine.api.log.RequestLogs;
+import com.google.appengine.api.log.dev.LocalLogService;
+import com.google.appengine.tools.development.testing.LocalLogServiceTestConfig;
+
 import teammates.common.util.Const;
 import teammates.ui.controller.AdminActivityLogPageAction;
+import teammates.ui.controller.ShowPageResult;
 
 public class AdminActivityLogPageActionTest extends BaseActionTest {
 
@@ -20,7 +28,24 @@ public class AdminActivityLogPageActionTest extends BaseActionTest {
     @Override
     @Test
     public void testExecuteAndPostProcess() {
+//        gaeSimulation.loginAsAdmin("admin");
+//        AdminActivityLogPageAction action = getAction();
+//        ShowPageResult result = getShowPageResult(action);
+//        System.out.println(result.getStatusMessage());
         
+        LocalLogService localLogService = LocalLogServiceTestConfig.getLocalLogService();
+        localLogService.addRequestInfo("test", "1", "123", "test", "test",
+                                        System.nanoTime() / 1000, System.nanoTime() / 1000, "test", "test", "test",
+                                        "test", true, 200, "test");
+        localLogService.addAppLogLine("123", 3333L, 1, "test");
+        
+        LogService logService = LogServiceFactory.getLogService();
+        LogQuery query = LogQuery.Builder.withDefaults();
+        query.includeAppLogs(true);
+        for (RequestLogs requestLog : logService.fetch(query)) {
+            System.out.println(requestLog.getCombined());
+            System.out.println(requestLog.getAppLogLines());
+        }
         //TODO: implement this
     }
 
