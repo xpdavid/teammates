e32a8c43963980ccff2dd66498416b046cd9bc07
Pu Xiao <xp.xiaopu@gmail.com>
2017-03-15 19:07:21 +0800

    Add custom check BooleanNameCheck

---
+++ static-analysis/checkstyle-lib/src/java/BooleanNameCheck.java
@@ -0,0 +1,69 @@
+package com.puppycrawl.tools.checkstyle.checks.naming;
+
+import java.util.Arrays;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class BooleanNameCheck extends AbstractCheck {
+
+    public static final String MSG = "Boolean variable name should start with %s";
+
+    public static final String[] DEFAULT_PREFIXES = new String[] {"is", "has", "can", "should"};
+
+    private String[] allowedPrefixes;
+
+    public BooleanNameCheck() {
+        allowedPrefixes = DEFAULT_PREFIXES;
+    }
+
+    public void setAllowedPrefixes(String[] values) {
+        if (values == null) {
+            return;
+        }
+        allowedPrefixes = values;
+    }
+
+    @Override
+    public int[] getDefaultTokens() {
+        return getAcceptableTokens();
+    }
+
+    @Override
+    public int[] getAcceptableTokens() {
+        return new int[] {TokenTypes.VARIABLE_DEF};
+    }
+
+    @Override
+    public int[] getRequiredTokens() {
+        return getAcceptableTokens();
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        if (!isBooleanVariableDefinition(ast)) {
+            return;
+        }
+
+        String variableName = getVariableName(ast);
+        for (String allowedPrefix : allowedPrefixes) {
+            if (variableName.startsWith(allowedPrefix)) {
+                return;
+            }
+        }
+
+        log(ast.getLineNo(), ast.getColumnNo(),
+                String.format(MSG, Arrays.toString(allowedPrefixes)), ast.getText());
+    }
+
+    private boolean isBooleanVariableDefinition(DetailAST ast) {
+        DetailAST variableType = ast.findFirstToken(TokenTypes.TYPE);
+        return variableType.findFirstToken(TokenTypes.LITERAL_BOOLEAN) != null;
+    }
+
+    private String getVariableName(DetailAST ast) {
+        return ast.findFirstToken(TokenTypes.IDENT).getText();
+    }
+
+}
